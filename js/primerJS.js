e4a1ee9bf3c3562f95e14ab1e906c2214fab708f
arnoldbgq <arnoldb.gq@gmail.com>
2026-04-16 18:13:50 -0500

    sint

--- js/primerJS.js
+++ js/primerJS.js
@@ -1,7 +1,7 @@
 // Console.log nos sirve para depurar y probar nuestras salidas
 console.log("Hola mundo desde JavaScript");
 //alert nos sirve para mostrarle un alert al usuario
-alert("Hola Mundo. Esto es una Alerta");
+alert("Hola Mundo. Esto es una Alerta")
 
 function cambiarCarita(){
     document.getElementById("carita").src="images/sad.png";

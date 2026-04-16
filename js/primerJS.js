733d00aea4a778bb107bf623456edc681f6716e5
arnoldbgq <arnoldb.gq@gmail.com>
2026-04-16 17:48:25 -0500

    aler

--- js/primerJS.js
+++ js/primerJS.js
@@ -1,4 +1,6 @@
-// Primer script en JavaScript
+// Console.log nos sirve para depurar y probar nuestras salidas
 console.log("Hola mundo desde JavaScript");
+//alert nos sirve para mostrarle un alert al usuario
 alert("Hola Mundo. Esto es una Alerta");
+
 

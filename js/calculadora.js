b6cbb265d0a2703f04ae272fc46dc6263414b4b2
arnoldbgq <arnoldb.gq@gmail.com>
2026-04-23 16:12:57 -0500

    este funciona bien, si lo malogras, restaura el archivo

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+ 
     document.getElementById("formCalculadora").addEventListener("submit", function(e){
         e.preventDefault();// evita que el formulario me vote de la pagina
 

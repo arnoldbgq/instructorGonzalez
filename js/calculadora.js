d5b965da9e220065f8f23b1670afc228b41e4d5a
arnoldbgq <arnoldb.gq@gmail.com>
2026-04-23 16:03:25 -0500

    c

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,7 +3,14 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("formCalculadora").addEventListener("submit", function(e)){
         e.preventDefault();// evita que el formulario me vote de la pagina
 
-        
+        let nume1 = parseFloat(document.getElementById("numero1").value);
+        let nume2 = parseFloat(document.getElementById("numero2").value);
+        let operacion = document.getElementById("operacion").value;
+        let resultado = document.getElementById("resultado");
+
+        if(isNaN(nume1)||isNaN(nume2)){
+            resultado.textContent = "Ingrese números válidos";
+        }
     }
 
 })

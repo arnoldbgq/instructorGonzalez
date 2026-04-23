87298b9c5e3df85e80bc6fa0bef230884fd7d088
arnoldbgq <arnoldb.gq@gmail.com>
2026-04-23 15:58:13 -0500

    error

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     document.getElementById("formCalculadora").addEventListener("submit", function(e)){
-        e.preventDefault();
+        e.preventDefault();// evita que el formulario me vote de la pagina
+
+        
     }
 
 })
